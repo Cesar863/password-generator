dd7b050a7107204ff290ca7a2bf240832eb75bc0
Cesar863 <90540881+Cesar863@users.noreply.github.com>
2021-11-07 17:39:55 -0500

    added progressive prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,37 +5,91 @@ var lowLetters = "abcdefghijklmnopqrstuvwxyz";
 var capLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var specialChars = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
-var passwordTxt = document.getElementById("password");
+var pwText = document.getElementById("password");
 var pwLength = document.getElementById("length");
 var pwNumbersIncluded = document.getElementById("numbers");
 var pwSpecialCharsIncluded = document.getElementById("special-characters");
 var pwCapIncluded = document.getElementById("capLetters");
 var pwLowIncluded = document.getElementById("lowLetters");
-var generateBtn = document.getElementById("generate");
+var genBtn = document.getElementById("generate");
+var d1 = document.getElementById("d1");
+var d2 = document.getElementById("d2");
+var d3 = document.getElementById("d3");
+var d4 = document.getElementById("d4");
+var d5 = document.getElementById("d5");
+var generateBtn = document.getElementById("finalgen");
 
-generateBtn.addEventListener("click",() => {
+function showDiv1() {
+  document.getElementById("passlength").style.display = "block";
+}
+
+function showDiv2() {
+  document.getElementById("incnum").style.display = "block";
+}
+
+function showDiv3() {
+  document.getElementById("symbols").style.display = "block";
+}
+
+function showDiv4() {
+  document.getElementById("cap").style.display = "block";
+}
+
+function showDiv5() {
+  document.getElementById("low").style.display = "block";
+}
+
+function showDiv5() {
+  document.getElementById("finalgen").style.display = "block";
+}
+
+genBtn.addEventListener("click", () => {
+  showDiv1();
+  document.getElementById("generate").style.display = "none";
+
+});
+
+d1.addEventListener("click", () => {
+  showDiv2();
+});
+
+d2.addEventListener("click", () => {
+  showDiv3();
+});
+d3.addEventListener("click", () => {
+  showDiv4();
+});
+d4.addEventListener("click", () => {
+  showDiv5();
+});
+d5.addEventListener("click", () => {
+  showDiv6();
+});
+
+generateBtn.addEventListener("click", () => {
   var characters = placeHolder;
   pwNumbersIncluded.checked ? (characters += numbers) : "";
   pwSpecialCharsIncluded.checked ? (characters += specialChars) : "";
   pwCapIncluded.checked ? (characters += capLetters) : "";
   pwLowIncluded.checked ? (characters += lowLetters) : "";
-  passwordTxt.value = generatePassword(pwLength.value, characters);
+  pwText.value = generatePassword(pwLength.value, characters);
 });
 
 var generatePassword = (pwLength, characters) => {
   var password = "";
   for (var i = 0; i < pwLength; i++) {
     password += characters.charAt(
-      Math.floor(Math.random()* characters.length)
+      Math.floor(Math.random() * characters.length)
     );
   }
+  window.alert("Here is your password!")
   return password;
 };
 
 var copyBtn = document.getElementById("copy");
 
 copyBtn.addEventListener("click", () => {
-  passwordTxt.select();
+  pwText.select();
   document.execCommand("copy");
   window.alert("Password Copied");
 });
